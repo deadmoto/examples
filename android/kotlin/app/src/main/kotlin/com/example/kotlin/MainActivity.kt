5f545ac7d8c14f7beb475a026326a3ed8471b506
Dmitry Berezhnoy <v-dberezhnoy@expedia.com>
2015-01-15 12:50:32 -0800

    Added Android Kotlin example project.

---
+++ android/kotlin/app/src/main/kotlin/com/example/kotlin/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kotlin
+
+import android.app.Activity
+import android.os.Bundle
+
+public class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
