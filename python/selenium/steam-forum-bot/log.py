cced3ee54b8070485e9c89c4e8b78768dbfc4838
Dmitry Berezhnoy <v-dberezhnoy@expedia.com>
2015-02-12 11:57:07 -0800

    Updated script to handle topics created but not stored in database.

--- python/selenium/steam-forum-bot/log.py
+++ python/selenium/steam-forum-bot/log.py
@@ -1,12 +1,11 @@
-import datetime
 import sqlite3
 
 
 class Log(object):
     def __init__(self):
         super(Log, self).__init__()
-        sql = 'CREATE TABLE if not exists log (game int, ts datetime)'
-        self.connection = sqlite3.connect('log.sqlite')
+        sql = 'CREATE TABLE if not exists log (game int, expires timestamp)'
+        self.connection = sqlite3.connect('log.sqlite', detect_types=sqlite3.PARSE_DECLTYPES)
         self.cursor = self.connection.cursor()
         self.cursor.execute(sql)
         self.connection.commit()
@@ -15,15 +14,14 @@ class Log(object):
         self.cursor.close()
         self.connection.close()
 
-    def insert(self, game):
+    def insert(self, game, expires):
         sql = 'INSERT INTO log VALUES (?, ?)'
-        self.cursor.execute(sql, (game, datetime.datetime.now()))
+        self.cursor.execute(sql, (game, expires))
         self.connection.commit()
 
-    def check(self, game):
-        sql = 'SELECT * FROM log WHERE game = ? and datetime(ts) > datetime(?)'
-        yesterday = datetime.datetime.now() + datetime.timedelta(days=-1)
-        return self.cursor.execute(sql, (game, yesterday)).fetchone() is not None
+    def select(self, game):
+        sql = 'SELECT expires FROM log WHERE game = ? ORDER BY expires DESC'
+        return self.cursor.execute(sql, (game,)).fetchone()
 
 
 log = Log()
--- python/selenium/steam-forum-bot/script.py
+++ python/selenium/steam-forum-bot/script.py
@@ -1,3 +1,7 @@
+from datetime import datetime, timedelta
+from re import search
+from time import strptime, mktime
+
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.common.by import By
@@ -12,9 +16,11 @@ profile = webdriver.FirefoxProfile("./firefox")
 driver = webdriver.Firefox(firefox_profile=profile)
 games = Inventory.games()
 for game in sorted(games.keys(), key=lambda g: len(games[g]), reverse=True):
-    if log.check(game):
-        print 'Topic for game #{} was created less than 24 hours ago'.format(game)
-        continue
+    expires = log.select(game)
+    if expires is not None:
+        if expires[0] > datetime.now():
+            print 'Topic for game #{} cannot be created till {}'.format(game, expires[0])
+            continue
     print 'Processing game #{}'.format(game)
     topic = '[H] ' + games[game].pop()
     while len(games[game]) > 0:
@@ -39,11 +45,21 @@ for game in sorted(games.keys(), key=lambda g: len(games[g]), reverse=True):
         driver.find_element_by_class_name('btn_green_white_innerfade').click()
         condition = expected_conditions.invisibility_of_element_located((By.CLASS_NAME, 'forum_topic_input'))
         wait.until(condition)
-        log.insert(game)
+        log.insert(game, datetime.now() + timedelta(days=1))
         print 'Created a topic for game #{}: {}'.format(game, topic.encode('utf_8'))
     except NoSuchElementException:
         print('Failed to create topic for game {}!'.format(game))
         print('URL: {}'.format(url))
     except TimeoutException:
-        print 'You cannot create one more topic!'.format(game)
+        try:
+            error = driver.find_element_by_class_name('forum_newtopic_error').text
+            match = search('[\w]{3}\s[\d]{1,2}\s@\s[\d]{1,2}:[\d]{2}[\w]{2}', error)
+            if match is None:
+                raise NoSuchElementException
+            group = match.group(0)
+            expires = strptime('{} {}'.format(datetime.now().year, group), '%Y %b %d @ %I:%M%p')
+            log.insert(game, datetime.fromtimestamp(mktime(expires)))
+            print 'Topic for game #{} cannot be created till {}'.format(game, datetime.fromtimestamp(mktime(expires)))
+        except NoSuchElementException:
+            print 'Topic for game #{} was not created!'.format(game)
 driver.quit()
